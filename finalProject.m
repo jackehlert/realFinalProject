0e24287ee57500f42b618fbdf66910d553f87eda
bebel2 <bebel2@huskers.unl.edu>
2021-04-30 16:20:24 -0500

    start of project

--- finalProject.m
+++ finalProject.m
@@ -1,7 +1,25 @@
 function [] = finalProject()
     global gui;
+    global R;
+    R = 8.314;
  gui.fig = figure('numbertitle', 'off', 'name', 'Van Der Waals Equation'); 
- gui.
+ gui.temperature = uicontrol('style','slider');
+ gui.temperatureLabel = uicontrol('style','text','string','slide to your temperature in Kelvin');
+ gui.criticalTemperature = uicontrol('style','edit');
+ gui.criticalPressure = uicontrol('style','edit');
+ gui.specificVolume = uicontrol('style','edit');
     
+
+end
+function [] = aVariable()
+
+end
+function [] = bVariable()
+
+end
+function [] = termOne()
+
+end
+function [] = termTwo()
 
 end
